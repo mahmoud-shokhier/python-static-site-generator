31889adaae064ff5e240bb9b472e92a24716efef
mahmoud-shokhier <mahmoud.aly.shokhier@gmail.com>
2022-07-20 17:24:16 +0200

    first commit module 3

---
+++ ssg/content.py
@@ -0,0 +1,11 @@
+import re 
+from yaml import load, FullLoader
+from collections.abc import Mapping
+
+class Content(Mapping):
+    __delimiter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
+    
+    # @classmethod
+    # def load(cls, string):
+        
