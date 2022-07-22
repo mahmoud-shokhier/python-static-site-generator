33733ffc3ef71502e78c286af9f11b494235ffda
mahmoud-shokhier <mahmoud.aly.shokhier@gmail.com>
2022-07-22 20:28:02 +0200

    fix function setter type

--- ssg/content.py
+++ ssg/content.py
@@ -26,8 +26,8 @@ class Content(Mapping):
        
     
     @type.setter
-    def type(self, value):
-        self.data["type"] = value
+    def type(self, type):
+        self.data["type"] = type
     
     def __getitem__(self, key):
         return self.data[key]
